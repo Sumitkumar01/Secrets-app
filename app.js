f2469e7d8ef7a04b8fd39c951d24bbe5281ec63e
Sumit Kumar <sk7627577@gmail.com>
2022-09-12 20:15:47 +0530

    level 1 Auhentication

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const bodyParser =require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.use(express.static("public"));//to use public folder
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine','ejs');
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+
+const userDataSchema = {
+  email:String,
+  password:String
+};
+
+const User = mongoose.model("User", userDataSchema);
+
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+
+app.post("/register", function(req,res){
+  const newUser = new User ({
+    email:req.body.username,
+    password:req.body.password
+  });
+  newUser.save(function(err){//use to save the userData to database.
+    if(err){
+      console.log(err);
+    }else{
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login",function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+//here we check email & password  of user.
+  User.findOne({email: username},function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+
+
+
+
+app.listen(3000,function(){
+  console.log("Server is started.");
+});
