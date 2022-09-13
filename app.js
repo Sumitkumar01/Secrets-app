d2666fa67dc294b1dbdc39955b6bce6268ac9a7e
Sumit Kumar <sk7627577@gmail.com>
2022-09-13 20:56:57 +0530

    level-4 bcrypt to use salt & hash the password.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser =require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");//to use hash+salt we use bcrypt library
+const saltRounds = 10;//to salt the password hash it
 
 const app = express();
 
@@ -41,31 +42,38 @@ app.get("/login",function(req,res){
 
 
 app.post("/register", function(req,res){
-  const newUser = new User ({
-    email:req.body.username,
-    password:md5(req.body.password)
-  });
-  newUser.save(function(err){//use to save the userData to database.
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  });
+
+  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    const newUser = new User ({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){//use to save the userData to database.
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
+  })
+
 });
 
 app.post("/login",function(req,res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 //here we check email & password  of user.
   User.findOne({email: username},function(err,foundUser){
     if(err){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password,foundUser.password,function(err,result){
+          if(result === true){
+            res.render("secrets");
+          }
+
+        });
       }
     }
   });
